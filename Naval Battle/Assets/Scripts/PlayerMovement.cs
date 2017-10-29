09667cb6b7d0d27e2a9b5a22aea0ce0080ff3d92
Martin Šoštar <martin_sostar@yahoo.de>
2017-10-29 22:50:37 +0100

    Player Movement Fixed

--- Naval Battle/Assets/Scripts/PlayerMovement.cs
+++ Naval Battle/Assets/Scripts/PlayerMovement.cs
@@ -9,8 +9,6 @@ public class PlayerMovement : MonoBehaviour {
     [SerializeField]
     private float Speed = 1.0f;
     [SerializeField]
-    private float MinSpeed = 0.0f;
-    [SerializeField]
     private float MaxSpeed = 3.0f;
 
     [Header("Turning")]
@@ -21,13 +19,14 @@ public class PlayerMovement : MonoBehaviour {
     [SerializeField]
     private float MaxTurningSpeed = 1.0f;
     [SerializeField]
-    private float MinTurningSpeed = 0.0f;
+    private float MaxTurningAngle = 35.0f;
 
     [Header("TEST Read Only")]
     public Vector3 CurrentMovement = Vector3.zero;
     public Vector3 CurrentAngularVelocity = Vector3.zero;
     public float MaxZSpeed = 0.0f;
     public float MaxXSpeed = 0.0f;
+    public Vector3 LocalMovement = Vector3.zero; 
 
     private float _movementForward;
     private float _movementTurning;
@@ -41,24 +40,40 @@ public class PlayerMovement : MonoBehaviour {
 
     private void Update() {
         _movementForward = Input.GetAxisRaw("Vertical");
-        _movementForward = Mathf.Clamp(_movementForward, 0.0f, 1.1f);
-        _movementTurning = Input.GetAxisRaw("Horizontal");
-        Vector3 turningMovement = new Vector3(_movementTurning * TurningSpeed, 0.0f, 0.0f);        
+        //_movementForward = Mathf.Clamp(_movementForward, 0.0f, 1.1f);
+        _movementTurning = Input.GetAxisRaw("Horizontal");  
 
         _rigidbody.AddForceAtPosition(_transform.right * TurningSpeed * _movementTurning, TurningPoint.position, ForceMode.Impulse);
-        _rigidbody.AddRelativeForce(_transform.forward * Speed * _movementForward, ForceMode.Force);
+        _rigidbody.AddForce(_transform.forward * Speed * _movementForward, ForceMode.Force);
 
-        Vector3 velocity = _rigidbody.velocity;     //nemoguce direktno pristupiti _rigidbody.velocity.z da se clamp-a u limit
+        /*Vector3 velocity = _rigidbody.velocity;     //nemoguce direktno pristupiti _rigidbody.velocity.z da se clamp-a u limit
         velocity.z = Mathf.Clamp(velocity.z, -MaxSpeed, MaxSpeed);
         velocity.x = Mathf.Clamp(velocity.x, -MaxSpeed, MaxSpeed);
-        _rigidbody.velocity = velocity;
+        _rigidbody.velocity = velocity;*/
 
+        
+        Vector3 localVelocity = _transform.InverseTransformDirection(_rigidbody.velocity);
+        localVelocity.z = Mathf.Clamp(localVelocity.z, 0.0f, MaxSpeed);
+        localVelocity.y = 0;
+        localVelocity.x = 0;
+        _rigidbody.velocity = _transform.TransformDirection(localVelocity);
+
+        /*
+        Vector3 rotation = _transform.localRotation.eulerAngles;
+        rotation.z = Mathf.Clamp(rotation.z, -MaxTurningAngle, MaxTurningAngle);
+        rotation.x = 0.0f;
+        _transform.localRotation = Quaternion.Euler(rotation);
+        */
+        
         Vector3 angularVelocity = _rigidbody.angularVelocity;
-        angularVelocity.y = Mathf.Clamp(angularVelocity.y, MinTurningSpeed, MaxTurningSpeed);
+        angularVelocity.y = Mathf.Clamp(angularVelocity.y, -MaxTurningSpeed, MaxTurningSpeed);
         angularVelocity.z = 0.0f;
         angularVelocity.x = 0.0f;
         _rigidbody.angularVelocity = angularVelocity;
+        
 
+        //TODO ocistiti ovo skupa s testnim read only varijablama
+        LocalMovement = localVelocity;
         CurrentAngularVelocity = _rigidbody.angularVelocity;
         CurrentMovement = _rigidbody.velocity;
         if(_rigidbody.velocity.z > MaxZSpeed) {
